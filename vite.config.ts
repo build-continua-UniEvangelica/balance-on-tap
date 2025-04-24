cda2597f5a703cc8f1bd9177cea7f243009620d1
Bertonsin <gustavobertonsin@hotmail.com>
2025-04-23 21:14:52 -0300

    fix: 🐛 fix cd config

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ export default defineConfig(({ mode }) => ({
     host: "::",
     port: 8080,
   },
-  base: 'balance-on-tap',
+  base: '/balance-on-tap/',
   plugins: [
     react(),
     mode === 'development' &&
