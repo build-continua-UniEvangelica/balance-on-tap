0c20bcdbcbf3e7cae78a830b31b13dfb06bf5050
Bertonsin <gustavobertonsin@hotmail.com>
2025-04-23 21:12:27 -0300

    chore: update deploy config

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ export default defineConfig(({ mode }) => ({
     host: "::",
     port: 8080,
   },
-  base: 'teste',
+  base: 'balance-on-tap',
   plugins: [
     react(),
     mode === 'development' &&
