450a44e839e3e91ffa29347be5ae4daf4abea5a3
Bertonsin <gustavobertonsin@hotmail.com>
2025-04-27 14:08:42 -0300

    refactor: ♻️ remove unused components

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,4 @@
 
-import { Toaster as Sonner } from "@/components/ui/sonner";
-import { Toaster } from "@/components/ui/toaster";
-import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Route, Routes } from "react-router-dom";
 import DashboardPage from "./pages/DashboardPage";
@@ -12,15 +9,11 @@ const queryClient = new QueryClient();
 
 const App = () => (
   <QueryClientProvider client={queryClient}>
-    <TooltipProvider>
-      <Toaster />
-      <Sonner />
-        <Routes>
-          <Route path="/" element={<Index />} />
-          <Route path="/dashboard" element={<DashboardPage />} />
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-    </TooltipProvider>
+    <Routes>
+      <Route path="/" element={<Index />} />
+      <Route path="/dashboard" element={<DashboardPage />} />
+      <Route path="*" element={<NotFound />} />
+    </Routes>
   </QueryClientProvider>
 );
 
