fa105eaae335b0b3197b8e157d1d007edb7b49fe
Bertonsin <gustavobertonsin@hotmail.com>
2025-04-27 20:21:06 -0300

    refactor: ♻️ remove firefox config

--- playwright.config.ts
+++ playwright.config.ts
@@ -45,10 +45,5 @@ export default defineConfig({
       name: 'chromium',
       use: { ...devices['Desktop Chrome'] },
     },
-
-    {
-      name: 'firefox',
-      use: { ...devices['Desktop Firefox'] },
-    },
   ],
 });
